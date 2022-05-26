a178f2c420f3de0242b71a037fb265c53f1c8809
Christian Escobar <christian.escobar@autocred.cl>
2022-05-26 17:47:40 -0400

    update routes

--- src/router/index.js
+++ src/router/index.js
@@ -8,10 +8,8 @@ import FAQ from '../views/preguntas-frecuentes.vue'
 import registro from '../views/registro.vue'
 import servicios from '../views/servicios.vue'
 import vendedores from '../views/vendedores.vue'
-import blog from "../views/blog.vue"
-import article from "../views/article.vue"
-
-
+import blog from '../views/blog.vue'
+import article from '../views/article.vue'
 
 const router = createRouter({
     history: createWebHistory(),
@@ -36,7 +34,7 @@ const router = createRouter({
         { path: '/servicios', name: 'Servicios', component: servicios, props: (route) => ({ ...route.params }) },
         { path: '/vendedores', name: 'Vendedores', component: vendedores },
         { path: '/blog', name: 'Blog', component: blog },
-        { path: '/blog/:id', name: 'Articulo', component: article, },
+        { path: '/blog/:id', name: 'Articulo', component: article },
 
 
         // { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
