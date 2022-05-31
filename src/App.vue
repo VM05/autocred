04fdece56e0ee8ba144aae278e68945941389b7e
Christian Escobar <christian.escobar@autocred.cl>
2022-05-31 18:44:02 -0400

    Add metatags default

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <script setup>
 import Navbar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
+import { useHead } from "@vueuse/head";
+
+useHead({
+  title: "Autocred Chile tu mejor copiloto",
+  meta: [
+    {
+      name: "description",
+      content:
+        "Autocred lo hace fácil. Postula simultáneamente tu crédito con diferentes crediticias y obtén una aprobación en segundos",
+    },
+  ],
+});
 </script>
 
 <template>
