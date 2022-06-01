3453fbc490dc042fbb4499ea87f9b7fe0dc23b04
Christian Escobar <christian.escobar@autocred.cl>
2022-06-01 11:03:00 -0400

    Add vite-radar & conditional dev/prod

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,47 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ViteRadar from 'vite-plugin-radar'
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  // server: {
-  //   host: true,
-  //   https: true
-  // }
+
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//     ViteRadar({
+//       enableDev: false,
+//       // Google Analytics tag injection
+//       analytics: [{
+//         id: 'G-W14GHXEK2S',
+//       }],
+//       pixel: {
+//         id: '6b6ae67a-e1b2-4508-9c12-629992ce2545',
+//       },
+//     })
+//   ]
+// })
+
+export default defineConfig(({ command, mode }) => {
+  if (mode === 'development') {
+    return {
+      plugins: [
+        vue(),
+      ]
+    }
+  } else {
+    return {
+      plugins: [
+        vue(),
+        ViteRadar({
+          // Google Analytics tag injection
+          analytics: [{
+            id: 'G-W14GHXEK2S',
+          }],
+          pixel: {
+            id: '6b6ae67a-e1b2-4508-9c12-629992ce2545',
+          },
+        })
+      ]
+    }
+  }
 })
