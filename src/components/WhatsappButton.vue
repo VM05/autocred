0fbc5f75f08a60153a23a3eb3b4c514de84c4b55
Victor Montiel <victor.montiel@autocred.cl>
2022-11-03 15:04:16 -0300

    cambios en el componente de whatsapp

--- src/components/WhatsappButton.vue
+++ src/components/WhatsappButton.vue
@@ -1,13 +1,16 @@
 <template>
-
-<!-- <div>
+  <!-- <div>
     <a  :href="`https://wa.me/${props.telefono}/`" target="_blank">
       <img src="../assets/img/ws.webp" alt="whatsapp autocred" title="whatsapp autocred" />
     </a>
   </div> -->
   <div>
     <a @click.prevent="isOpen = true">
-      <img src="../assets/img/ws.webp" alt="whatsapp autocred" title="whatsapp autocred" />
+      <img
+        src="../assets/img/ws.webp"
+        alt="whatsapp autocred"
+        title="whatsapp autocred"
+      />
     </a>
   </div>
 
@@ -20,92 +23,110 @@
           nuestro ejecutivo
         </p>
         <div class="flex justify-center contenedor-formulario flex-col">
-        <Loading  medium v-if="isLoading"/>
-        <form id="form_popup_whatsapp" @submit.prevent="enviarFormulario" v-if="!isLoading">
-          <div class="text-sm">
-            <Input
-            label="Nombre"
-            id="nombre"
-            placeholder="Nombre"
-            @textvalue="(e) => (formularioWs.nombre_completo = e)"
-            class="py-1"
-            />
-          </div>
-          <div class="text-sm relative">
-            <Input
-            label="Teléfono"
-            id="Telefono"
-            isPhone
-            placeholder="Teléfono"
-            @update:text="(e) => (formularioWs.telefono = e)"
-            :value="
-            formularioWs.telefono ? formularioWs.telefono : ''
-            "
-            @keypress="onlyNumber"
-            @textvalue="(e) => checkTelefono(e)"
-            class="py-1"
-            />
-            <Paragraph
-                        class=" w-full -bottom-6 md:-bottom-6 left-1/2  text-red-700 justify-self-center grid-flow-row text-center"
-                      v-if="warningPhone"
-                      >
-                        El teléfono debe contener al menos 9 digitos
-                      </Paragraph>
-          </div>
+          <Loading medium v-if="isLoading" />
+          <form
+            id="form_popup_whatsapp"
+            @submit.prevent="enviarFormulario"
+            v-if="!isLoading"
+          >
+            <div class="text-sm">
+              <Input
+                label="Nombre"
+                id="nombre"
+                placeholder="Nombre"
+                @textvalue="(e) => (formularioWs.nombre_completo = e)"
+                class="py-1"
+              />
+            </div>
+            <div class="text-sm relative">
+              <Input
+                label="Teléfono"
+                id="Telefono"
+                isPhone
+                placeholder="Teléfono"
+                @update:text="(e) => (formularioWs.telefono = e)"
+                :value="formularioWs.telefono ? formularioWs.telefono : ''"
+                @keypress="onlyNumber"
+                @textvalue="(e) => checkTelefono(e)"
+                class="py-1"
+              />
+              <Paragraph
+                class="w-full -bottom-6 md:-bottom-6 left-1/2 text-red-700 justify-self-center grid-flow-row text-center"
+                v-if="warningPhone"
+              >
+                El teléfono debe contener al menos 9 digitos
+              </Paragraph>
+            </div>
 
-          <div class="text-sm relative">
-            <InputEmail
-            placeholder="Ingresa tu correo de contacto"
-            label="Correo electrónico"
-            id="email"
-            @textvalue="(e) => (formularioWs.email = e)"
-            class="py-1"
-          />
-          </div>
-          <div class="text-sm" v-if="!warningFinanciamiento">
-          <InputRut 
-          label="Rut"
-          id="RUT"
-          placeholder="Ingresa tu rut"
-          class="w-full py-1 md:text-sm"
-          @textvalue="(e) => (formularioWs.dni= e)"
-          @keypress="onlyRut"
-          />
-          </div>
-         
-          <div class="md:text-sm">
-            <CheckServicios class="py-1"
-            label="¿Qué servicios necesitas?"
-            columnas
-            @update:checkServicios="(e) => handleCheck(e)"
-            />
-          </div>
-          <div class="popup_button_container py-2">
-            <Button type="submit" whatsapp  :disabled="errorForm" text="Iniciar Chat" class="rounded-3xl py-1"/>
-            <Button danger @click="isOpen = false,formularioWs.telefono = '', warningPhone = false " text="Cerrar Chat" class="rounded-3xl" />
-          </div>
-        </form>
-      </div>
+            <div class="text-sm relative">
+              <InputEmail
+                placeholder="Ingresa tu correo de contacto"
+                label="Correo electrónico"
+                id="email"
+                @textvalue="(e) => (formularioWs.email = e)"
+                class="py-1"
+              />
+            </div>
+            <div class="text-sm" v-if="!warningFinanciamiento">
+              <InputRut
+                label="Rut"
+                id="RUT"
+                placeholder="Ingresa tu rut"
+                class="w-full py-1 md:text-sm"
+                @textvalue="(e) => (formularioWs.dni = e)"
+                @keypress="onlyRut"
+              />
+            </div>
+
+            <div class="md:text-sm">
+              <CheckServicios
+                class="py-1"
+                label="¿Qué servicios necesitas?"
+                columnas
+                @update:checkServicios="(e) => handleCheck(e)"
+              />
+            </div>
+            <div class="popup_button_container py-2">
+              <Button
+                type="submit"
+                whatsapp
+                :disabled="errorForm"
+                text="Iniciar Chat"
+                class="rounded-3xl py-1"
+              />
+              <Button
+                danger
+                @click="
+                  (isOpen = false),
+                    (formularioWs.telefono = ''),
+                    (warningPhone = false)
+                "
+                text="Cerrar Chat"
+                class="rounded-3xl"
+              />
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, reactive, watch } from "vue";
-import Button from "../components/Button.vue"
+import { ref, reactive, watch, onMounted } from "vue";
+import Button from "../components/Button.vue";
 import Input from "../components/Form/Input.vue";
 import InputRut from "../components/Input-Rut.vue";
 import { formEmpty, validateEmail } from "../assets/helpers/validate";
 import CheckServicios from "./CheckServicios.vue";
 import InputEmail from "./Input-Email.vue";
-import axios from 'axios';
+import axios from "axios";
 import Loading from "../components/Loading.vue";
 import { useContactoStore } from "../stores/contacto";
 import { gestion, URL_GOGEMA } from "../assets/helpers/API";
 import qs from "qs";
 import { validateRut, RutFormat, formatRut } from "@fdograph/rut-utilities";
-import Paragraph from '../components/Paragraph.vue'
+import Paragraph from "../components/Paragraph.vue";
 
 const props = defineProps({
   telefono: String,
@@ -122,37 +143,54 @@ const formularioWs = reactive({
   nombre_completo: "",
   email: "",
   telefono: "",
-  dni:"0",
+  dni: "0",
   mensaje: "desde whatsapp",
-  procedencia_id: useUtms.utm_procedenciaId || 116,
+  procedencia_id: 116,
   tipo_contacto: useUtms.utm_tipoProcedencia || "Whatsapp",
   servicios: "",
   utm_source: useUtms.utm_source || "Whatsapp",
   utm_medium: useUtms.utm_medium || "web",
   utm_campaign: useUtms.utm_campaign || "web",
   canal_atencion: gestion[2].name,
-})
+});
 
-const enviarFormulario = async() => {
-
+const enviarFormulario = async () => {
   isLoading.value = true;
   try {
     const resp = await axios.post(URL_GOGEMA, qs.stringify(formularioWs));
     if (await resp.data.message) {
       isLoading.value = false;
-      if(warningFinanciamiento.value == true){
-        window.open(`https://api.whatsapp.com/send?phone=${props.telefono}&text=Hola mi nombre es ${formularioWs.nombre_completo}, mi correo electrónico es ${formularioWs.email}, mi número de teléfono es ${formularioWs.telefono} y me gustaría consultar por los servicios de: ${formularioWs.servicios}`, "_self")
-      }else{
-      window.open(`https://api.whatsapp.com/send?phone=${props.telefono}&text=Hola mi nombre es ${formularioWs.nombre_completo}, mi correo electrónico es ${formularioWs.email}, mi Rut es ${formatRut(formularioWs.dni, RutFormat.DOTS_DASH)}, mi número de teléfono es ${formularioWs.telefono} y me gustaría consultar por los servicios de: ${formularioWs.servicios}`, "_self")
+      if (warningFinanciamiento.value == true) {
+        window.open(
+          `https://api.whatsapp.com/send?phone=${props.telefono}&text=Hola mi nombre es ${formularioWs.nombre_completo}, mi correo electrónico es ${formularioWs.email}, mi número de teléfono es ${formularioWs.telefono} y me gustaría consultar por los servicios de: ${formularioWs.servicios}`,
+          "_self"
+        );
+      } else {
+        window.open(
+          `https://api.whatsapp.com/send?phone=${
+            props.telefono
+          }&text=Hola mi nombre es ${
+            formularioWs.nombre_completo
+          }, mi correo electrónico es ${
+            formularioWs.email
+          }, mi Rut es ${formatRut(
+            formularioWs.dni,
+            RutFormat.DOTS_DASH
+          )}, mi número de teléfono es ${
+            formularioWs.telefono
+          } y me gustaría consultar por los servicios de: ${
+            formularioWs.servicios
+          }`,
+          "_self"
+        );
       }
     }
   } catch (error) {
-      console.log(error)
+    console.log(error);
     if (error.response.data.message) {
       isLoading.value = false;
     }
   }
-
 };
 
 const onlyRut = ($event) => {
@@ -167,38 +205,44 @@ const handleCheck = (e) => {
   formularioWs.servicios = formated;
 };
 
-watch(formularioWs, ()=>{
- 
-  if(formularioWs.servicios.includes('Financiamientos')){
-    warningFinanciamiento.value= false;
-    
-  }else{
+watch(formularioWs, () => {
+  if (formularioWs.servicios.includes("Financiamientos")) {
+    warningFinanciamiento.value = false;
+  } else {
     warningFinanciamiento.value = true;
-    formularioWs.dni = '0'
+    formularioWs.dni = "0";
   }
 
-  if(!warningFinanciamiento.value){
-
-    if(formEmpty(formularioWs) || !validateEmail(formularioWs.email) || formularioWs.telefono.length < 9 || !validateRut(formularioWs.dni) || formularioWs.dni == '0'){
-    errorForm.value = true;
-  }else{
-    errorForm.value = false;
+  if (!warningFinanciamiento.value) {
+    if (
+      formEmpty(formularioWs) ||
+      !validateEmail(formularioWs.email) ||
+      formularioWs.telefono.length < 9 ||
+      !validateRut(formularioWs.dni) ||
+      formularioWs.dni == "0"
+    ) {
+      errorForm.value = true;
+    } else {
+      errorForm.value = false;
+    }
+  } else {
+    if (
+      formEmpty(formularioWs) ||
+      !validateEmail(formularioWs.email) ||
+      formularioWs.telefono.length < 9
+    ) {
+      errorForm.value = true;
+    } else {
+      errorForm.value = false;
+    }
   }
-  }else{
+});
 
-    if(formEmpty(formularioWs) || !validateEmail(formularioWs.email) || formularioWs.telefono.length < 9){
-    errorForm.value = true;
-  }else{
-    errorForm.value = false;
-  }
-  }
-
-})
-
-watch(isOpen,()=>{
-  isOpen.value && window.innerWidth < 768? document.documentElement.style.overflow = 'hidden': document.documentElement.style.overflow = 'auto';
-
-})
+watch(isOpen, () => {
+  isOpen.value && window.innerWidth < 768
+    ? (document.documentElement.style.overflow = "hidden")
+    : (document.documentElement.style.overflow = "auto");
+});
 
 const onlyNumber = ($event) => {
   const validNumbers = /[0-9]+/;
@@ -216,22 +260,17 @@ const checkTelefono = (e) => {
     }
   }
 };
-
 </script>
 <style>
-.prueba{
+.prueba {
   overflow: hidden;
 }
-
 </style>
 
 <style scoped>
-
-.contenedor-formulario{
+.contenedor-formulario {
   min-height: 400px;
-
 }
-
 
 a {
   width: 100%;
@@ -281,7 +320,7 @@ form textarea {
 }
 
 .popup_title_text {
-  background-color: #25D366;
+  background-color: #25d366;
   padding: 0px 30px;
   display: block;
 }
@@ -312,8 +351,6 @@ form textarea {
   z-index: 10;
   animation: appear 0.3s ease;
 }
-
-
 
 @keyframes appear {
   from {
@@ -346,15 +383,13 @@ form textarea {
   max-width: 350px;
 }
 
-@media (max-width: 500px){
-  .modal-container{
+@media (max-width: 500px) {
+  .modal-container {
     bottom: 5%;
     left: 50%;
     transform: translateX(-50%);
     right: unset;
     width: 100%;
-
   }
 }
-
 </style>
