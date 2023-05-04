dcbd1c6ffb8dbfbc4390877c9fa0e19a4d80918b
victor Montiel <victorhmg05@gmail.com>
2023-05-03 21:14:35 -0400

    cambios

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -19,33 +19,18 @@
           id="password"
           @update:text="(e) => (loginForm.password = e)"
         />
-        <Paragraph
-          v-if="errorForm"
-          class="text-red-700 justify-self-center mb-2"
-        >
+        <Paragraph v-if="errorForm" class="text-red-700 justify-self-center mb-2">
           Rut y/o Contraseña ingresados no son válidos
         </Paragraph>
-        <Button1
-          text="Ingresar"
-          secondary
-          class="justify-self-center w-fit"
-          type="submit"
-        />
-        <a
-          :href="PASSWORD_RESET"
-          class="text-primary-900 underline font-semibold text-base"
-        >
+        <Button1 text="Ingresar" secondary class="justify-self-center w-fit" type="submit" />
+        <a :href="PASSWORD_RESET" class="text-primary-900 underline font-semibold text-base">
           Olvide mi contraseña
         </a>
       </form>
       <div class="grid items-center gap-4">
         <Paragraph> Aun no tienes tu cuenta en Autocred?</Paragraph>
         <router-link to="/registro" class="justify-self-center w-fit">
-          <Button1
-            text="Crear Cuenta"
-            outlineSecondary
-            class="justify-self-center w-fit"
-          />
+          <Button1 text="Crear Cuenta" outlineSecondary class="justify-self-center w-fit" />
         </router-link>
       </div>
     </div>
@@ -53,54 +38,50 @@
 </template>
 
 <script setup>
-import { reactive, ref, watch } from "vue";
-import Heading1 from "../components/Heading.vue";
-import InputRut1 from "../components/Input-Rut.vue";
-import Input from "./Form/Input.vue";
-import Button1 from "./Button.vue";
-import Paragraph from "../components/Paragraph.vue";
-import axios from "axios";
-import { useRoute } from "vue-router";
+  import { reactive, ref, watch } from 'vue';
+  import Heading1 from '../components/Heading.vue';
+  import InputRut1 from '../components/Input-Rut.vue';
+  import Input from './Form/Input.vue';
+  import Button1 from './Button.vue';
+  import Paragraph from '../components/Paragraph.vue';
+  import axios from 'axios';
+  import { useRoute } from 'vue-router';
 
-import {
-  LOGIN_URL,
-  LOGIN_URL_TOKEN,
-  PASSWORD_RESET,
-} from "../assets/helpers/API";
-import Loading from "./Loading.vue";
-import { isObjEmpty } from "../assets/helpers/validate";
+  import { LOGIN_URL, LOGIN_URL_TOKEN, PASSWORD_RESET } from '../assets/helpers/API';
+  import Loading from './Loading.vue';
+  import { isObjEmpty } from '../assets/helpers/validate';
 
-const route = useRoute();
-const data = ref(route.params);
-const loading = ref(false);
-const errorForm = ref(false);
-const loginForm = reactive({
-  dni: "",
-  password: "",
-  remember_me: "1",
-});
+  const route = useRoute();
+  const data = ref(route.params);
+  const loading = ref(false);
+  const errorForm = ref(false);
+  const loginForm = reactive({
+    dni: '',
+    password: '',
+    remember_me: '1',
+  });
 
-const fillFormFromParams = () => {
-  if (!isObjEmpty(data.value)) {
-    loginForm.dni = data.value.rut;
-  }
-};
-
-const handleLogin = async () => {
-  await fillFormFromParams();
-  loading.value = true;
-  try {
-    const response = await axios.post(LOGIN_URL, loginForm);
-    if (await response.data.success) {
-      errorForm.value = false;
-      window.open(LOGIN_URL_TOKEN(response.data.token_id), "_self");
-    } else {
+  const fillFormFromParams = () => {
+    if (!isObjEmpty(data.value)) {
+      loginForm.dni = data.value.rut;
     }
-  } catch (error) {
-    loading.value = false;
-    errorForm.value = true;
-  }
-};
+  };
+
+  const handleLogin = async () => {
+    await fillFormFromParams();
+    loading.value = true;
+    try {
+      const response = await axios.post(LOGIN_URL, loginForm);
+      if (await response.data.success) {
+        errorForm.value = false;
+        window.open(LOGIN_URL_TOKEN(response.data.token_id), '_self');
+      } else {
+      }
+    } catch (error) {
+      loading.value = false;
+      errorForm.value = true;
+    }
+  };
 </script>
 
 <style scoped></style>
