8a4c49c2be30f761ae14e2b81a5f2825528b37d6
victor Montiel <victorhmg05@gmail.com>
2023-05-04 22:08:28 -0400

    prueba

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,50 @@
 <script setup>
-import { useHead } from "@vueuse/head";
-import { useContactoStore } from "./stores/contacto";
-import { onBeforeMount, watch } from "vue";
-import WhatsappButton from "./components/WhatsappButton.vue";
-import ws from './components/ws.vue'
+  import { useHead } from '@vueuse/head';
+  import { useContactoStore } from './stores/contacto';
+  import { onBeforeMount, watch } from 'vue';
+  import WhatsappButton from './components/WhatsappButton.vue';
+  import ws from './components/ws.vue';
+  import axios from 'axios';
 
-useHead({
-  title: "Autocred Chile tu mejor copiloto",
-  meta: [
-    {
-      name: "description",
-      content:
-        "Autocred lo hace fácil. Postula simultáneamente tu crédito con diferentes crediticias y obtén una aprobación en segundos",
-    },
-  ],
-});
+  useHead({
+    title: 'Autocred Chile tu mejor copiloto',
+    meta: [
+      {
+        name: 'description',
+        content:
+          'Autocred lo hace fácil. Postula simultáneamente tu crédito con diferentes crediticias y obtén una aprobación en segundos',
+      },
+    ],
+  });
 
+  const storeContacto = useContactoStore();
 
+  onBeforeMount(async () => {
+    const { data } = await axios.get(
+      'https://proyecto-calendario-2ee58-default-rtdb.firebaseio.com/fechas.json'
+    );
 
-const storeContacto = useContactoStore();
+    console.log(Object.keys(data));
+    let arreglo = [];
+    for (let id of Object.keys(data)) {
+      arreglo.push({ id, ...data[id] });
+    }
 
-onBeforeMount(() => {
-  storeContacto.fetchUtms();
-  storeContacto.getProcedencia();
-  storeContacto.isMobile();
-  storeContacto.encuentraDispositivo();
-});
+    console.log(arreglo);
 
-// watch(storeContacto,()=>{
-//   if(storeContacto.modal_open){
-//     document.documentElement.style.overflow = "hidden"
-//   }else{
-//     document.documentElement.style.overflow = "auto"
-//   }
-// })
+    storeContacto.fetchUtms();
+    storeContacto.getProcedencia();
+    storeContacto.isMobile();
+    storeContacto.encuentraDispositivo();
+  });
+
+  // watch(storeContacto,()=>{
+  //   if(storeContacto.modal_open){
+  //     document.documentElement.style.overflow = "hidden"
+  //   }else{
+  //     document.documentElement.style.overflow = "auto"
+  //   }
+  // })
 </script>
 
 <template>
