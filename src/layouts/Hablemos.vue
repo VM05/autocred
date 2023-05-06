f034e4cb066d3b2a38b1619fe2d607b34a7d35f1
victor Montiel <victorhmg05@gmail.com>
2023-05-05 20:23:15 -0400

    eliminacion de credenciales

--- src/layouts/Hablemos.vue
+++ src/layouts/Hablemos.vue
@@ -1,21 +1,14 @@
 <template>
   <div class="py-6 md:py-12 px-6 md:px-12 md:mb-12 mb-6">
     <Heading1 content="Solicita tu servicio con nosotros" class="mb-12" />
-    <div
-      class="bg-transparent md:bg-hablemos-100 rounded-xl card p-0 md:px-6 md:py-7 md:shadow-xl"
-    >
-      <Heading1
-        content="Formulario Contacto"
-        headingType="h3"
-        class="border-b pb-4"
-      />
+    <div class="bg-transparent md:bg-hablemos-100 rounded-xl card p-0 md:px-6 md:py-7 md:shadow-xl">
+      <Heading1 content="Formulario Contacto" headingType="h3" class="border-b pb-4" />
       <div class="grid place-content-center my-20" v-if="alerts">
         <Loading v-if="isLoading" />
         <div v-if="isSuccess" class="text-center">
           <Paragraph class="font-bold">Gracias!</Paragraph>
           <Paragraph class="mb-8"
-            >Estamos buscando a tu copiloto y este se comunicará contigo a la
-            brevedad.
+            >Estamos buscando a tu copiloto y este se comunicará contigo a la brevedad.
           </Paragraph>
           <Button text="Volver" outlinePrimary="" @click="btnBack" />
         </div>
@@ -23,9 +16,7 @@
           <Paragraph class="font-bold text-secondary-900"
             >Ocurrio un problema con los datos ingresados</Paragraph
           >
-          <Paragraph class="mb-8 text-secondary-900"
-            >Intenta de nuevo
-          </Paragraph>
+          <Paragraph class="mb-8 text-secondary-900">Intenta de nuevo </Paragraph>
           <Button text="Volver" outlineSecondary @click="btnBack" />
         </div>
       </div>
@@ -35,11 +26,7 @@
         id="form"
         v-show="!isSuccess && !isError && !isLoading"
       >
-        <input
-          type="hidden"
-          name="user_servicios"
-          :value="formContacto.servicios"
-        />
+        <input type="hidden" name="user_servicios" :value="formContacto.servicios" />
 
         <input type="hidden" name="user_errorform" :value="errorForm" />
         <input type="hidden" name="user_browser" :value="useUtms.browserName" />
@@ -108,12 +95,7 @@
             @update:textArea="(e) => (formContacto.mensaje = e)"
           />
           <div class="flex justify-end">
-            <ButtonVue
-              text="Enviar"
-              outlinePrimary
-              type="submit"
-              :disabled="warnings.isWarning"
-            />
+            <ButtonVue text="Enviar" outlinePrimary type="submit" :disabled="warnings.isWarning" />
           </div>
         </div>
       </form>
@@ -122,221 +104,216 @@
 </template>
 
 <script setup>
-import Heading1 from "../components/Heading.vue";
-import Input from "../components/Form/Input.vue";
-import InputEmail from "../components/Input-Email.vue";
-// import SelectServicios from "../components/SelectServicios.vue";
-import CheckServicios from "../components/CheckServicios.vue";
-import TextArea1 from "../components/TextArea.vue";
-import ButtonVue from "../components/Button.vue";
-import SelectGestion from "../components/SelectGestion.vue";
-import { reactive, ref, watch, onMounted, getCurrentInstance } from "vue";
-import { formEmpty, validateEmail } from "../assets/helpers/validate";
-import { gestion, servicios, URL_GOGEMA } from "../assets/helpers/API";
-import axios from "axios";
-import qs from "qs";
-import Loading from "../components/Loading.vue";
-import Paragraph from "../components/Paragraph.vue";
-import Button from "../components/Button.vue";
-import { useContactoStore } from "../stores/contacto";
-import { useRoute, useRouter } from "vue-router";
-import emailjs from "@emailjs/browser";
-import InputRut from "../components/Input-Rut.vue";
-import { validateRut } from "@fdograph/rut-utilities";
+  import Heading1 from '../components/Heading.vue';
+  import Input from '../components/Form/Input.vue';
+  import InputEmail from '../components/Input-Email.vue';
+  // import SelectServicios from "../components/SelectServicios.vue";
+  import CheckServicios from '../components/CheckServicios.vue';
+  import TextArea1 from '../components/TextArea.vue';
+  import ButtonVue from '../components/Button.vue';
+  import SelectGestion from '../components/SelectGestion.vue';
+  import { reactive, ref, watch, onMounted, getCurrentInstance } from 'vue';
+  import { formEmpty, validateEmail } from '../assets/helpers/validate';
+  import { gestion, servicios, URL_GOGEMA } from '../assets/helpers/API';
+  import axios from 'axios';
+  import qs from 'qs';
+  import Loading from '../components/Loading.vue';
+  import Paragraph from '../components/Paragraph.vue';
+  import Button from '../components/Button.vue';
+  import { useContactoStore } from '../stores/contacto';
+  import { useRoute, useRouter } from 'vue-router';
+  import emailjs from '@emailjs/browser';
+  import InputRut from '../components/Input-Rut.vue';
+  import { validateRut } from '@fdograph/rut-utilities';
 
-const route = useRoute();
-const router = useRouter();
-const isLoading = ref(false);
-const isSuccess = ref(false);
-const isError = ref(false);
-const alerts = ref(false);
-const warnings = reactive({
-  warningTelefono: false,
-  warningServicios: true,
-  isWarning: true,
-});
-const errorForm = ref("uno de los campos esta vacio");
-const direccion_sitio = window.location.href;
-const warningFinanciamiento = ref(true);
+  const route = useRoute();
+  const router = useRouter();
+  const isLoading = ref(false);
+  const isSuccess = ref(false);
+  const isError = ref(false);
+  const alerts = ref(false);
+  const warnings = reactive({
+    warningTelefono: false,
+    warningServicios: true,
+    isWarning: true,
+  });
+  const errorForm = ref('uno de los campos esta vacio');
+  const direccion_sitio = window.location.href;
+  const warningFinanciamiento = ref(true);
 
-const onlyRut = ($event) => {
-  const validRut = /[^kK0-9]/g;
-  if (validRut.test($event.key)) {
-    $event.preventDefault();
-  }
-};
+  const onlyRut = ($event) => {
+    const validRut = /[^kK0-9]/g;
+    if (validRut.test($event.key)) {
+      $event.preventDefault();
+    }
+  };
 
-//Use Params
-const useUtms = useContactoStore();
+  //Use Params
+  const useUtms = useContactoStore();
 
-const props = defineProps({
-  selectService: {
-    default: 0,
-    type: Number,
-  },
-});
-const formContacto = reactive({
-  nombre_completo: "",
-  email: "",
-  telefono: "",
-  mensaje: "",
-  procedencia_id: useUtms.utm_procedenciaId || 103,
-  tipo_contacto: useUtms.utm_tipoProcedencia || "web",
-  servicios: "",
-  canal_atencion: gestion[0].name,
-  utm_source: useUtms.utm_source || "web",
-  utm_medium: useUtms.utm_medium || "web",
-  utm_campaign: useUtms.utm_campaign || "web",
-  sucursal_id: 1,
-  dni: "0",
-});
+  const props = defineProps({
+    selectService: {
+      default: 0,
+      type: Number,
+    },
+  });
+  const formContacto = reactive({
+    nombre_completo: '',
+    email: '',
+    telefono: '',
+    mensaje: '',
+    procedencia_id: useUtms.utm_procedenciaId || 103,
+    tipo_contacto: useUtms.utm_tipoProcedencia || 'web',
+    servicios: '',
+    canal_atencion: gestion[0].name,
+    utm_source: useUtms.utm_source || 'web',
+    utm_medium: useUtms.utm_medium || 'web',
+    utm_campaign: useUtms.utm_campaign || 'web',
+    sucursal_id: 1,
+    dni: '0',
+  });
 
-// const modal = ref(false);
-const isFormComplete = ref(formEmpty(formContacto));
-const isEmailValid = ref(validateEmail(formContacto.email));
+  // const modal = ref(false);
+  const isFormComplete = ref(formEmpty(formContacto));
+  const isEmailValid = ref(validateEmail(formContacto.email));
 
-const handleForm = async (e) => {
-  e.preventDefault();
+  const handleForm = async (e) => {
+    e.preventDefault();
 
-  if (!formEmpty(formContacto)) {
-    await sendFormGoGema();
+    if (!formEmpty(formContacto)) {
+      await sendFormGoGema();
 
-    if (isSuccess.value == true) {
-      const formulario = document.getElementById("form");
-      formulario.reset();
-      router.push({ path: route.path, hash: contactRouteSuccess() });
+      if (isSuccess.value == true) {
+        const formulario = document.getElementById('form');
+        formulario.reset();
+        router.push({ path: route.path, hash: contactRouteSuccess() });
 
-      const span = document.querySelectorAll(".mt-1 span");
-      span.forEach((span) => {
-        if (span.style.display === "none") {
-          span.click();
-        }
-      });
+        const span = document.querySelectorAll('.mt-1 span');
+        span.forEach((span) => {
+          if (span.style.display === 'none') {
+            span.click();
+          }
+        });
+      } else {
+        enviarMail();
+        router.push({ path: route.path, hash: contactRouteIncomplete() });
+      }
+    }
+  };
+  const contactRouteSuccess = () => {
+    let res = '#confirmacion';
+    res = res + '-' + route.name.replace(' ', '-');
+    return res.toLowerCase();
+  };
+
+  const contactRouteIncomplete = () => {
+    let res = '#no-informado';
+    res = res + '-' + route.name.replace(' ', '-');
+    return res.toLowerCase();
+  };
+
+  const sendFormGoGema = async () => {
+    isLoading.value = true;
+    try {
+      const resp = await axios.post(URL_GOGEMA, qs.stringify(formContacto));
+      if (await resp.data.message) {
+        isLoading.value = false;
+        isSuccess.value = true;
+      }
+    } catch (error) {
+      errorForm.value = error.response.request.response;
+      if (error.response.data.message) {
+        isLoading.value = false;
+        isError.value = true;
+      }
+    }
+  };
+
+  const componentKey = ref(0);
+
+  const forceRerender = () => {
+    componentKey.value += 1;
+  };
+
+  const btnBack = () => {
+    forceRerender();
+    isSuccess.value = false;
+    isError.value = false;
+  };
+  const handleCheck = (e) => {
+    let formated = e.value.join(',');
+    formContacto.servicios = formated;
+  };
+
+  watch(formContacto, () => {
+    if (formContacto.servicios.includes('Financiamientos')) {
+      warningFinanciamiento.value = false;
     } else {
-      enviarMail();
-      router.push({ path: route.path, hash: contactRouteIncomplete() });
+      warningFinanciamiento.value = true;
+      formContacto.dni = '0';
     }
-  }
-};
-const contactRouteSuccess = () => {
-  let res = "#confirmacion";
-  res = res + "-" + route.name.replace(" ", "-");
-  return res.toLowerCase();
-};
 
-const contactRouteIncomplete = () => {
-  let res = "#no-informado";
-  res = res + "-" + route.name.replace(" ", "-");
-  return res.toLowerCase();
-};
+    isFormComplete.value = formEmpty(formContacto);
+    isEmailValid.value = validateEmail(formContacto.email);
 
-const sendFormGoGema = async () => {
-  isLoading.value = true;
-  try {
-    const resp = await axios.post(URL_GOGEMA, qs.stringify(formContacto));
-    if (await resp.data.message) {
-      isLoading.value = false;
-      isSuccess.value = true;
-    }
-  } catch (error) {
-    errorForm.value = error.response.request.response;
-    if (error.response.data.message) {
-      isLoading.value = false;
-      isError.value = true;
-    }
-  }
-};
-
-const componentKey = ref(0);
-
-const forceRerender = () => {
-  componentKey.value += 1;
-};
-
-const btnBack = () => {
-  forceRerender();
-  isSuccess.value = false;
-  isError.value = false;
-};
-const handleCheck = (e) => {
-  let formated = e.value.join(",");
-  formContacto.servicios = formated;
-};
-
-watch(formContacto, () => {
-  if (formContacto.servicios.includes("Financiamientos")) {
-    warningFinanciamiento.value = false;
-  } else {
-    warningFinanciamiento.value = true;
-    formContacto.dni = "0";
-  }
-
-  isFormComplete.value = formEmpty(formContacto);
-  isEmailValid.value = validateEmail(formContacto.email);
-
-  // if (formContacto.mensaje.length == 0) {
-  //   formContacto.mensaje =
-  //     formContacto.nombre_completo +
-  //     " desea evaluar servicios de " +
-  //     formContacto.servicios;
-  // }
-  if (formContacto.telefono.length > 0 && formContacto.telefono.length < 12) {
-    warnings.warningTelefono = true;
-  } else {
-    warnings.warningTelefono = false;
-  }
-  if (formContacto.servicios.length > 0) {
-    warnings.warningServicios = false;
-  } else {
-    warnings.warningServicios = true;
-  }
-
-  if (!warningFinanciamiento.value) {
-    if (
-      warnings.warningTelefono == false &&
-      warnings.warningServicios == false &&
-      isEmailValid.value == true &&
-      isFormComplete.value == false &&
-      formContacto.mensaje != "" &&
-      validateRut(formContacto.dni)
-    ) {
-      warnings.isWarning = false;
+    // if (formContacto.mensaje.length == 0) {
+    //   formContacto.mensaje =
+    //     formContacto.nombre_completo +
+    //     " desea evaluar servicios de " +
+    //     formContacto.servicios;
+    // }
+    if (formContacto.telefono.length > 0 && formContacto.telefono.length < 12) {
+      warnings.warningTelefono = true;
     } else {
-      warnings.isWarning = true;
+      warnings.warningTelefono = false;
     }
-  } else {
-    if (
-      warnings.warningTelefono == false &&
-      warnings.warningServicios == false &&
-      isEmailValid.value == true &&
-      isFormComplete.value == false &&
-      formContacto.mensaje != ""
-    ) {
-      warnings.isWarning = false;
+    if (formContacto.servicios.length > 0) {
+      warnings.warningServicios = false;
     } else {
-      warnings.isWarning = true;
+      warnings.warningServicios = true;
     }
-  }
 
-  console.log(validateRut(formContacto.dni));
-  console.log(formContacto.dni);
-});
+    if (!warningFinanciamiento.value) {
+      if (
+        warnings.warningTelefono == false &&
+        warnings.warningServicios == false &&
+        isEmailValid.value == true &&
+        isFormComplete.value == false &&
+        formContacto.mensaje != '' &&
+        validateRut(formContacto.dni)
+      ) {
+        warnings.isWarning = false;
+      } else {
+        warnings.isWarning = true;
+      }
+    } else {
+      if (
+        warnings.warningTelefono == false &&
+        warnings.warningServicios == false &&
+        isEmailValid.value == true &&
+        isFormComplete.value == false &&
+        formContacto.mensaje != ''
+      ) {
+        warnings.isWarning = false;
+      } else {
+        warnings.isWarning = true;
+      }
+    }
 
-watch([isLoading, isSuccess, isError], () => {
-  isLoading.value == true || isSuccess.value == true || isError.value == true
-    ? (alerts.value = true)
-    : (alerts.value = false);
-});
+    console.log(validateRut(formContacto.dni));
+    console.log(formContacto.dni);
+  });
 
-const enviarMail = () => {
-  const formulario = document.getElementById("form");
-  emailjs.sendForm(
-    "service_pgv1taa",
-    "template_seiqd1b",
-    formulario,
-    "Jm7OFS9JSVMS2XjET"
-  );
-};
+  watch([isLoading, isSuccess, isError], () => {
+    isLoading.value == true || isSuccess.value == true || isError.value == true
+      ? (alerts.value = true)
+      : (alerts.value = false);
+  });
+
+  const enviarMail = () => {
+    const formulario = document.getElementById('form');
+    // emailjs.sendForm('service_pgv1taa', 'template_seiqd1b', formulario, 'Jm7OFS9JSVMS2XjET');
+  };
 </script>
 
 <style scoped></style>
